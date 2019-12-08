adc98e14ffe73c8e4b658db287a4b2bc31c99d19
jram99 <ramjac13@gmail.com>
2019-12-07 19:29:01 -0500

    First commit

--- IoTClient.py
+++ IoTClient.py
@@ -1,11 +1,12 @@
-import socket
-import time
+import socket, time
+
+print("Welcome to your home security system\n")
 
 
 #if __name__ == "main":
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("10.17.111.161", 80))
-s.send(b'GET /sensors HTTP/1.1\r\n\r\n')
-time.sleep(1)
-reply = s.recv(1024)
-print(reply)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: #creates a socket s
+    s.connect(("10.17.111.161", 80))
+    s.send(b'GET /sensors HTTP/1.1\r\n\r\n')
+    time.sleep(1)
+    reply = s.recv(1024)
+    print(reply)
